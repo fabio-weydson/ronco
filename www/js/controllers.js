9cc8ec1aea8cfb93dde782da443f0c616ab5eacc
Weydson <weydson@github.com>
2018-07-29 18:05:11 -0300

    update

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -43,8 +43,8 @@ angular.module('starter.controllers', [])
 
 .controller('HomeCtrl', function($scope) {
   $scope.items = [
-    { title: '<b>Espaço BALI</b> colorindo nossas noites de Sexta-Feira aqui na Marulho! Vem conhecer nosso novo ambiente e curtir toda vibe desse lugar mágico ao som de Natiruts & Charlie Brown Jr, por Stefano Marques. Sexta (3) as 19H30 na Marulho. Ingressos antecipados já disponíveis nas lojas Ferrovia Iguatemi & Benfica!!', id: 1, src: '../fotos/nov1.jpg', data: '29 de Julho' },
-    { title: '<b>REVEILLON RONCO DO MAR BALI MMXIX.</b> Vem passar a virada de ano no paraíso e desfrutar dos nossos novos ambientes com vista para o mar. Vem celebrar a vida, abraçar quem ama e sorrir com intensidade por cada dia vivido. Reúne a família, convida os amigos e vem sentir toda a Positividade do Ronco do mar.', id: 2, src:'../fotos/nov2.jpg', data: '28 de Julho' }
+    { title: '<b>Espaço BALI</b> colorindo nossas noites de Sexta-Feira aqui na Marulho! Vem conhecer nosso novo ambiente e curtir toda vibe desse lugar mágico ao som de Natiruts & Charlie Brown Jr, por Stefano Marques. Sexta (3) as 19H30 na Marulho. Ingressos antecipados já disponíveis nas lojas Ferrovia Iguatemi & Benfica!!', id: 1, src: 'fotos/nov1.jpg', data: '29 de Julho' },
+    { title: '<b>REVEILLON RONCO DO MAR BALI MMXIX.</b> Vem passar a virada de ano no paraíso e desfrutar dos nossos novos ambientes com vista para o mar. Vem celebrar a vida, abraçar quem ama e sorrir com intensidade por cada dia vivido. Reúne a família, convida os amigos e vem sentir toda a Positividade do Ronco do mar.', id: 2, src:'fotos/nov2.jpg', data: '28 de Julho' }
   ];
 })
 
